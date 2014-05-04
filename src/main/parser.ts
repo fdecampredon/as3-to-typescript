1c04a242841ea9cda57950f9a7820ffd90668efd
François de Campredon <francois.de.campredon@gmail.com>
2014-05-04 12:47:33 +0200

    add lineman to parser for better error handling

--- src/main/parser.ts
+++ src/main/parser.ts
@@ -56,6 +56,114 @@ function endsWith(string: string, suffix: string) {
     return string.indexOf(suffix, string.length - suffix.length) !== -1;
 };
 
+var CARRIAGE_RETURN = 13,
+    LINE_FEED = 10,
+    LINE_SEPARATOR = 0x2028,
+    PARAGRAPH_SEPARATOR = 0x2029,
+    NEXT_LINE = 0x0085;
+
+function isAnyLineBreakCharacter(c: number): boolean {
+    return c === LINE_FEED ||
+           c === CARRIAGE_RETURN ||
+           c === NEXT_LINE ||
+           c === LINE_SEPARATOR ||
+           c === PARAGRAPH_SEPARATOR;
+}
+
+function getLengthOfLineBreak(text: string, index: number): number {
+    var c = text.charCodeAt(index);
+
+    // common case - ASCII & not a line break
+    if (c > CARRIAGE_RETURN && c <= 127) {
+        return 0;
+    }
+
+    if (c === CARRIAGE_RETURN) {
+        var next = index + 1;
+        return (next < text.length) && LINE_FEED === text.charCodeAt(next) ? 2 : 1;
+    }
+    else if (isAnyLineBreakCharacter(c)) {
+        return 1;
+    }
+    else {
+        return 0;
+    }
+}
+
+function buildLineMap(text: string): number[] {
+    var length = text.length;
+
+    // Corner case check
+    if (0 === length) {
+        var result = new Array<number>();
+        result.push(0);
+        return result;
+    }
+
+    var position = 0;
+    var index = 0;
+    var arrayBuilder = new Array<number>();
+    var lineNumber = 0;
+
+    // The following loop goes through every character in the text. It is highly
+    // performance critical, and thus inlines knowledge about common line breaks
+    // and non-line breaks.
+    while (index < length) {
+        var c = text.charCodeAt(index);
+        var lineBreakLength: number;
+
+        // common case - ASCII & not a line break
+        if (c > 13 && c <= 127) {
+            index++;
+            continue;
+        }
+        else if (c === 13 && index + 1 < length && text.charCodeAt(index + 1) === 10 ) {
+            lineBreakLength = 2;
+        }
+        else if (c === 10 ) {
+            lineBreakLength = 1;
+        }
+        else {
+            lineBreakLength = getLengthOfLineBreak(text, index);
+        }
+
+        if (0 === lineBreakLength) {
+            index++;
+        }
+        else {
+            arrayBuilder.push(position);
+            index += lineBreakLength;
+            position = index;
+            lineNumber++;
+        }
+    }
+
+    // Create a start for the final line.  
+    arrayBuilder.push(position);
+
+    return arrayBuilder;
+}
+
+function getLineAndCharacterFromPosition(position: number, lineStarts: number[]): { col: number; line:number } {
+    if (position < 0 || position > this.length) {
+        throw Error("invalid position" + position);
+    }
+
+    var lineNumber: number = -1;
+    if (position === this.length) {
+        lineNumber = lineStarts.length - 1;
+    } else {
+        for (var i = 0; i < lineStarts.length; i++) {
+            if (lineStarts[i] > position) {
+                break;
+            }
+            lineNumber = i;
+        }
+        lineNumber = lineNumber > 0 ? lineNumber : 0;
+    }
+    return { line: lineNumber, col: position - lineStarts[lineNumber] };
+}
+
 /**
  * @author xagnetti
  */
@@ -68,6 +176,7 @@ class AS3Parser {
     private isInFor: boolean;
     private scn: AS3Scanner;
     private tok: Token;
+    private lineMap: number[];
 
     /**
      * 
@@ -83,8 +192,11 @@ class AS3Parser {
      * java.lang.String[])
      */
     public buildAst(filePath: string, content: string): Node {
+        this.lineMap = buildLineMap(content);
         return this.parseFile(filePath, content);
     }
+    
+    
 
 
     private nextToken(ignoreDocumentation: boolean= false): void {
@@ -392,8 +504,10 @@ class AS3Parser {
                 new Position(this.tok.index, this.tok.getColumn()),
                 fileName,
                 text);*/
+            
+            var pos = getLineAndCharacterFromPosition(this.tok.index, this.lineMap);
             throw new Error('unexpected token : ' +
-                this.tok.text + '(' + this.tok.index + ')' +
+                this.tok.text + '(' + pos.line + ',' + pos.col + ')' +
                 ' in file ' + this.fileName +
                 'expected: ' + text
                 );
