1ad1e82fdb1b4013c5daa800861be5ba13fce136
François de Campredon <francois.de.campredon@gmail.com>
2014-05-04 12:47:53 +0200

    single now always take 'file.as'

--- test/single.js
+++ test/single.js
@@ -7,12 +7,11 @@ var AS3Parser = require('../lib/parser'),
 
 var parser = new AS3Parser();
 
-var fileName = process.argv[2];
 
-var content = fs.readFileSync(path.join(__dirname ,'single', fileName  + '.as'), 'UTF-8' );
-var ast = parser.buildAst(fileName + '.as', content);
+var content = fs.readFileSync(path.join(__dirname ,'single', 'file.as'), 'UTF-8' );
+var ast = parser.buildAst('file.as', content);
 var emitter = new Emitter(ast, content);
 
-fs.writeFileSync(path.join(__dirname ,'single', fileName  + '.ast.json'), JSON.stringify(ast, null, 4));
-fs.writeFileSync(path.join(__dirname ,'single', fileName + '.ts'),  emitter.emit());
+fs.writeFileSync(path.join(__dirname ,'single', 'file.ast.json'), JSON.stringify(ast, null, 4));
+fs.writeFileSync(path.join(__dirname ,'single', 'file.ts'),  emitter.emit());
 
